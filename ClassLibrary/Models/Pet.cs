229ae0c2d50bd324ce3ddb011a13aab372eb2cce
Lushi <vankovc@gmail.com>
2021-11-20 16:02:07 -0500

    added class library

---
+++ ClassLibrary/Models/Pet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ClassLibrary.Models
+{
+    public partial class Pet
+    {
+        public Pet()
+        {
+            Comments = new HashSet<Comment>();
+        }
+
+        public int PetId { get; set; }
+        public string PetName { get; set; }
+        public string PetType { get; set; }
+        public string PetImage { get; set; }
+        public string Breed { get; set; }
+        public string LastSeen { get; set; }
+        public string Description { get; set; }
+        public string Contact { get; set; }
+
+        public virtual ICollection<Comment> Comments { get; set; }
+    }
+}
---
+++ ClassLibrary/Models/ReunitePetsDbContext.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace ClassLibrary.Models
+{
+    public partial class ReunitePetsDbContext : DbContext
+    {
+        public ReunitePetsDbContext()
+        {
+        }
+
+        public ReunitePetsDbContext(DbContextOptions<ReunitePetsDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Comment> Comments { get; set; }
+        public virtual DbSet<Pet> Pets { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Comment>(entity =>
+            {
+                entity.ToTable("Comment");
+
+                entity.Property(e => e.Comment1)
+                    .IsRequired()
+                    .HasMaxLength(500)
+                    .HasColumnName("Comment");
+
+                entity.Property(e => e.CommentDate).HasColumnType("datetime");
+
+                entity.Property(e => e.Commenter)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.HasOne(d => d.Pet)
+                    .WithMany(p => p.Comments)
+                    .HasForeignKey(d => d.PetId);
+            });
+
+            modelBuilder.Entity<Pet>(entity =>
+            {
+                entity.ToTable("Pet");
+
+                entity.Property(e => e.Breed).HasMaxLength(20);
+
+                entity.Property(e => e.Contact).HasMaxLength(50);
+
+                entity.Property(e => e.Description).HasMaxLength(500);
+
+                entity.Property(e => e.LastSeen)
+                    .IsRequired()
+                    .HasMaxLength(200);
+
+                entity.Property(e => e.PetImage).HasMaxLength(500);
+
+                entity.Property(e => e.PetName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.PetType)
+                    .IsRequired()
+                    .HasMaxLength(10);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
